49856d69aade0a8c560fe9221786cd0860c2ffbf
Nimira43 <n.rabone72@aol.com>
2026-07-07 15:13:48 +0100

    Created two new files - one to handle Towers logic, the other to handle the shaders. This will replace the same logic currently in Maze.jsx Not hooked up yet.

---
+++ src/components/Towers.jsx
@@ -0,0 +1,33 @@
+// Separated from Maze.jsx - this Towers is not yet hooked up
+
+export default function Towers({ towers, towerHeight = 10 }) {
+  if (!towers.length) return null
+
+  return (
+    <>
+      {towers.map((t, i) => (
+        <group key={i} position={[t.x, 0, t.z]}>
+
+          <mesh position={[0, towerHeight / 2, 0]}>
+            <cylinderGeometry args={[0.55, 0.7, towerHeight, 16]} />
+            <meshStandardMaterial
+              color='#1a1d21'
+              roughness={0.4}
+              metalness={0.8}
+            />
+          </mesh>
+
+          <mesh position={[0, towerHeight + 0.15, 0]}>
+            <torusGeometry args={[0.5, 0.08, 12, 24]} />
+            <meshStandardMaterial
+              color='#ff4500'
+              emissive='#ff4500'
+              emissiveIntensity={1.5}
+            />
+          </mesh>
+
+        </group>
+      ))}
+    </>
+  )
+}
